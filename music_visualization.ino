51541ec7ff713be20be5ed4d4b5db41af054f11a
ken <kentanaka000@gmail.com>
2019-01-08 16:09:52 +0900

    three freq ranges

--- music_visualization.ino
+++ music_visualization.ino
@@ -3,11 +3,11 @@
 
 //FFT constants
 #define SAMPLES 128             //Must be a power of 2
-#define SAMPLING_FREQUENCY 8000 //Hz, must be less than 10000 due to ADC
+#define SAMPLING_FREQUENCY 1000 //Hz, must be less than 10000 due to ADC
 #define SAMPLE_PIN 5
 
 //LED constants
-#define LED_PIN     9
+#define LED_PIN     3
 #define NUM_LEDS    11
 #define BRIGHTNESS  32
 #define LED_TYPE    WS2811
@@ -17,6 +17,8 @@
 CRGB leds[NUM_LEDS];
 CRGB off[NUM_LEDS];
 int brightness = 32;
+int dimPeriod = 10;
+unsigned long dimTimer;
 
 
 //FFT vars
@@ -34,13 +36,18 @@ double prevReal[SAMPLES];
 double baseAvg;
 double prevBase;
 int baseCount;
-double otherAvg;
-double prevOther;
-int otherCount;
+double midAvg;
+double midPrevAvg;
+int midCount;
+double highAvg;
+double highPrevAvg;
+int highCount;
 
 //designs
-CRGB designA[NUM_LEDS] {CRGB::Red, CRGB::White, CRGB::Red, CRGB::White,CRGB::Red, CRGB::White,CRGB::Red, CRGB::White} ;
-CRGB designB[NUM_LEDS] {CRGB::Red, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Purple};
+CRGB designA[NUM_LEDS] {CRGB::Red, CRGB::White, CRGB::Red, CRGB::White,CRGB::Red, CRGB::White,CRGB::Red, CRGB::White, CRGB::Red} ;
+CRGB designB[NUM_LEDS] {CRGB::Red, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Purple, CRGB::Red, CRGB::Orange, CRGB::Yellow};
+CRGB designC[NUM_LEDS] {CRGB::White,CRGB::White,CRGB::White,CRGB::White,CRGB::White,CRGB::White,CRGB::White,CRGB::White,CRGB::White};
+
  
 void setup() {
     Serial.begin(115200);
@@ -51,9 +58,9 @@ void setup() {
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
     FastLED.setBrightness(  BRIGHTNESS );
 
-    
-    
+    dimTimer = millis();
 }
+
  
 void loop() {
    
@@ -64,6 +71,8 @@ void loop() {
      
         vReal[i] = analogRead(SAMPLE_PIN);
         vImag[i] = 0;
+
+        timedDim();
      
         while(micros() < (microseconds + sampling_period_us)){
         }
@@ -82,7 +91,6 @@ void loop() {
 
     // Peak Results
 //    Serial.println(peak);     //Print out what frequency is the most dominant.
-//    Serial.println(vReal[peakBin], 1);  // value of the peak
 
 
     // Show all outputs 
@@ -102,41 +110,57 @@ void loop() {
 
     for (int i = 0; i < (SAMPLES/2); i++) {
       int freq = (i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES;
-      if (freq < 250 && freq > 100) {
+      if (freq < 100 && freq > 30) {
         baseAvg += vReal[i];
         baseCount++;
       }
-      if (freq > 500) {
-        otherAvg += vReal[i];
-        otherCount++;
+      if (freq > 120 && freq < 250) {
+        midAvg += vReal[i];
+        midCount++;
       }
-
+      if (freq > 250) {
+        highAvg += vReal[i];
+        highCount++;
+      }
 
     }
       baseAvg /= baseCount;
-      otherAvg /= otherCount;
-    if (otherAvg > prevOther * 3 && otherAvg > 100 && peak > 500) {
-      patternA();
-    }
+      midAvg /= midCount;
+      highAvg /= highCount;
+      
     if (baseAvg > prevBase * 2 && baseAvg > 150) {
       patternB();
+    }
+    else if (midAvg > midPrevAvg * 1.5 && midAvg > 150) {
+      patternA();
+    }
+    else if (highAvg > highPrevAvg * 1.5 && highAvg > 120) {
+      patternC();
     }
 
     
     prevBase = baseAvg;
-    prevOther = otherAvg;
+    midPrevAvg = midAvg;
+    highPrevAvg = highAvg;
     baseCount = 0;
-    otherCount = 0;
+    midCount = 0;
+    highCount = 0;
     baseAvg = 0;
-    otherAvg = 0;
+    midAvg = 0;
+    highAvg = 0;
+//
+//    while(1);
+}
 
-    if (brightness > 0) {
-      brightness -=2;
-      
-      FastLED.setBrightness(  brightness );
-      FastLED.show();
-    }
+void timedDim() {
+  if (millis() - dimTimer > dimPeriod && brightness > 0) {
+    brightness --;
     
+    FastLED.setBrightness(  brightness );
+    FastLED.show();
+    dimTimer = millis();
+    
+  }
 }
 
 
@@ -150,6 +174,13 @@ void patternA() {
 void patternB() {
   for (int i = 0; i < NUM_LEDS;i++) {
     leds[i] = designB[i];
+  }
+  brightness = BRIGHTNESS;
+}
+
+void patternC() {
+  for (int i = 0; i < NUM_LEDS;i++) {
+    leds[i] = designC[i];
   }
   brightness = BRIGHTNESS;
 }
